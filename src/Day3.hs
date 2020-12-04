6799960d02f95016c99ebc8439a8b7656725875f
mauro <maurobalbi@bluewin.ch>
2020-12-04 20:02:57 +0100

    Day3 Part2

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day3
     ( 
         part1,
         part2
     ) where
 
+
 import Day2(safeHead)
 import Control.Monad
 import Data.Char
 import Data.List
+
+import Data.String.QQ
 import Text.Parsec
 import Text.Parsec.String
 
@@ -25,6 +30,19 @@ parseLine = manyTill parseTerrain $ eof <|> void newline
 parseTerrain :: Parser Terrain
 parseTerrain = char '#' *> pure Tree <|> char '.' *> pure Snow
 
+
+findTrees :: (Int, Int) -> [[Terrain]] -> [Terrain]
+findTrees slope input = filter (== Tree) $ passTerrain slope input
+
+passTerrain :: (Int, Int)-> [[a]] -> [a]
+passTerrain slope = pass slope . map cycle
+
+pass:: (Int, Int) -> [[a]] -> [a]
+pass slope input = foldr (\x y-> head (drop (fst slope * length y) x) : y ) [] $ reverse $ everyf (snd slope) input
+
+solve1 :: [[Terrain]] -> String
+solve1 input = show . length $ findTrees (3, 1) input
+
 part1 ::  String -> String
 part1 input = output
     where 
@@ -32,32 +50,39 @@ part1 input = output
             Right r -> solve1 r
             Left e -> show e
 
-solve1 :: [[Terrain]] -> String
-solve1 input = show . length $ findTrees 3 input
+everyf :: Int -> [a] -> [a]
+everyf n xs = case xs of
+              (y:ys) -> y : everyf n (drop (n-1) ys)
+              [] -> []
 
-findTrees :: Int -> [[Terrain]] -> [Terrain]
-findTrees slope input = filter (== Tree) $ passTerrain slope input
+slopes :: [(Int, Int)]
+slopes = [(1,1), (3,1), (5,1), (7,1), (1, 2)]
 
-passTerrain :: Int-> [[a]] -> [a]
-passTerrain slope = pass slope . (map cycle)
+slopeFunctions :: [[[Terrain]] -> Int]
+slopeFunctions = map treeCount slopes 
+    where
+        treeCount = \x -> length . findTrees x
 
-pass:: Int -> [[a]] -> [a]
-pass slope input = foldr (\x y-> (head (drop (slope * length y) x)) : y ) [] $ reverse input
-
-nthTerrain :: Int -> [Terrain] -> Terrain
-nthTerrain n input = head . drop n $ cycle input
+solve2 input = show . product $ slopeFunctions <*> pure input
 
 part2 ::  String -> String
 part2 input = output
     where 
         output = case parseInput input of
-            Right r -> solve1 r
+            Right r -> solve2 r
             Left e -> show e
 
-solve2 :: (Eq b, Num b) => [b] -> b -> [b]
-solve2 i test = 
-  do x <- i
-     y <- i
-     z <- i
-     guard ((x + y + z) == test)
-     pure (x * y * z)
+testInput :: String
+testInput = [s|
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+|]
